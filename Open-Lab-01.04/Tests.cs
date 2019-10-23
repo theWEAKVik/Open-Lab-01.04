57eb250746ce7188fc5ff97ceded7e020c466be8
Peter Milovcik <peter.milovcik@gmail.com>
2019-10-23 12:35:22 +0200

    Initial commit

---
+++ Open-Lab-01.04/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Open_Lab_01._04
+{
+    public class Tests
+    {
+        [TestCase(10, 5, 25)]
+        [TestCase(1, 10, 5)]
+        [TestCase(100, 50, 2500)]
+        [TestCase(1, 2, 1)]
+        [TestCase(2, 4, 4)]
+        public void Area(double foundation, double height, double result)
+        {
+            var triangle = new Triangle();
+            Assert.That(triangle.Area(foundation, height), Is.EqualTo(result));
+        }
+    }
+}
---
+++ Open-Lab-01.04/Triangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_01._04
+{
+    public class Triangle
+    {
+        public double Area(double foundation, double height)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
