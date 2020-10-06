2ad0ee09959bc5b6bdd4b847a1112bf48780a0b8
Viktor <krpectv57@gmail.com>
2020-10-06 10:53:00 +0200

    *

--- Open-Lab-01.04/Triangle.cs
+++ Open-Lab-01.04/Triangle.cs
@@ -6,7 +6,7 @@ namespace Open_Lab_01._04
     {
         public double Area(double foundation, double height)
         {
-            throw new NotImplementedException();
+            return foundation*height/2 ;
         }
     }
 }
